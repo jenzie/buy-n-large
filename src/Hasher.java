dace10127ed84e962ad5324ec33a6ad22df6d5a6
Jenny Zhen <jenny.zhen@rit.edu>
2014-02-20 15:13:21 -0500

    Refactored hash code into Hasher.

---
+++ src/Hasher.java
@@ -0,0 +1,65 @@
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+/**
+ author: Jenny Zhen
+ date: 02.20.14
+ language: Java
+ file: Hasher.java
+ assignment: BuyNLarge
+ http://www.cs.rit.edu/~wrc/courses/csci251/projects/1/
+ */
+
+public class Hasher {
+    public Hashtable<String, String> plaintextToHash(ArrayList<String> plaintextData) {
+        Hashtable<String, String> hashData = new Hashtable<String, String>();
+        for(String entry : plaintextData)
+            hashData.put(entry, byteArrayToHexString(getHash(entry)));
+        return hashData;
+    }
+
+    private byte[] getHash(String data) {
+        MessageDigest messageDigest = null;
+        byte[] result = new byte[0];
+
+        try {
+            messageDigest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            result = data.getBytes("UTF-8");
+
+            // Hash 100k times.
+            for (int i = 0; i < 100000; i++) {
+                if (messageDigest != null) {
+                    messageDigest.update(result);
+                    result = messageDigest.digest();
+                }
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    private String byteArrayToHexString(byte[] data) {
+        StringBuilder hexString = new StringBuilder();
+        for (byte piece : data) {
+            // Convert each byte to hex, while ANDing with 0xFF to get the least significant byte (masking).
+            String hex = Integer.toHexString(0xFF & piece);
+
+            // Check each byte for missing leading zeroes.
+            if (hex.length() == 1)
+                hexString.append('0');
+
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+}
