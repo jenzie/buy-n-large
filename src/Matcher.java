d5c6a0e4a3de10eb200f52f064999aa28488471c
Jenny Zhen <jenny.zhen@rit.edu>
2014-02-20 15:14:13 -0500

    Added code to match a hash with dictionary plaintext : hash values.

---
+++ src/Matcher.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ author: Jenny Zhen
+ date: 02.20.14
+ language: Java
+ file: Matcher.java
+ assignment: BuyNLarge
+ http://www.cs.rit.edu/~wrc/courses/csci251/projects/1/
+ */
+
+public class Matcher {
+    public String getMatch(String hash, HashMap<String, String> database) {
+        if (database.containsValue(hash)) {
+            for (Map.Entry<String, String> entry : database.entrySet()) {
+                if(hash.equals(entry.getValue()))
+                    return entry.getKey();
+            }
+        }
+        return null;
+    }
+}
